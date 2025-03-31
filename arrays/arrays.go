1374eab0ec355b7c26eaae349873ee1820e76972
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2025-03-31 20:19:41 +0530

    Learned slice

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,9 +1,20 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+
+	lengthOfSlice := len(numbersToSum)
+	sum := make([]int, lengthOfSlice)
+
+	for i, slice := range numbersToSum {
+		sum[i] = Sum(slice)
 	}
 	return sum
 }
